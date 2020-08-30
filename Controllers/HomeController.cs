09c12f54559301631a6e5b7d8008c88e08b70793
Daniel Falkner <bmw@740i.de>
2020-08-30 17:57:10 +0200

    Code format

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,16 +20,17 @@ namespace dotnet.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        private Cloud cloud; 
+        private Cloud cloud;
         private List<Cloud> clouds = new List<Cloud>();
         public const string SessionKeyName = "_CloudEnv";
 
-        private JsonSerializerOptions options; 
+        private JsonSerializerOptions options;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            options =  new JsonSerializerOptions {
+            options = new JsonSerializerOptions
+            {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true
             };
@@ -43,7 +44,7 @@ namespace dotnet.Controllers
 
         public IActionResult Index()
         {
-      
+
             var jsonModel = getServiceTagsModel();
 
             ViewData["changeNumber"] = jsonModel.changeNumber;
@@ -52,32 +53,34 @@ namespace dotnet.Controllers
             ViewBag.serviceTags = jsonModel.values;
 
             return View();
-        
+
         }
 
         public IActionResult getPrefixes(string id)
         {
-            
+
             var jsonModel = getServiceTagsModel();
 
-            String addressPrefixes = ""; 
+            String addressPrefixes = "";
             String filename = "prefixes.json";
 
-            foreach (ValuesModel values in jsonModel.values) {
-                if (values.id.Equals(id)) {
+            foreach (ValuesModel values in jsonModel.values)
+            {
+                if (values.id.Equals(id))
+                {
                     addressPrefixes = JsonSerializer.Serialize(values.properties);
                     filename = jsonModel.cloud + "." + values.id + ".json";
                 }
             }
-            
+
             var file = Encoding.ASCII.GetBytes(addressPrefixes);
-            return File(file,  "application/json", filename);             
+            return File(file, "application/json", filename);
 
         }
         [HttpGet("/getOPNsenseUrlTable/{env}/{id}")]
         public string getOPNsenseUrlTable(string id, string env)
         {
-                      
+
             var jsonModel = getServiceTagsModel(env);
 
             var opnsenseout = new System.Text.StringBuilder();
@@ -85,16 +88,18 @@ namespace dotnet.Controllers
             opnsenseout.AppendLine("; " + Request.GetDisplayUrl());
             opnsenseout.AppendLine("; Cloud: " + jsonModel.cloud + " - Changenumber: " + jsonModel.changeNumber);
 
-            foreach (ValuesModel values in jsonModel.values) {
-                if (values.id.Equals(id)) {
+            foreach (ValuesModel values in jsonModel.values)
+            {
+                if (values.id.Equals(id))
+                {
                     opnsenseout.AppendLine("; Service: " + values.id + " - Changenumber: " + values.properties.changeNumber);
                     foreach (string net in values.properties.addressPrefixes)
                     {
                         opnsenseout.AppendLine(net + " ; " + values.id);
-                    } 
+                    }
                 }
             }
-                        
+
             return opnsenseout.ToString();
 
         }
@@ -102,60 +107,70 @@ namespace dotnet.Controllers
         public IActionResult Update()
         {
 
-            try {
-                
+            try
+            {
+
                 UrlModel model = new UrlModel();
                 model.updateClouds(clouds);
-            
-            } catch (Exception e) {
-                ViewData["error"]  = e.ToString();
+
+            }
+            catch (Exception e)
+            {
+                ViewData["error"] = e.ToString();
             }
 
             return View();
 
         }
 
-        public IActionResult About() 
+        public IActionResult About()
         {
             return View();
         }
-        public IActionResult changeCloud(string env) {
+        public IActionResult changeCloud(string env)
+        {
 
             Response.Cookies.Append(SessionKeyName, env);
-            
+
             return RedirectToAction("Index");
 
         }
 
         private ServiceTagsModel getServiceTagsModel(string env)
         {
-            
+
             ViewData["env"] = env;
-            cloud =  clouds.Find(x => x.CloudName.Contains(env));
+            cloud = clouds.Find(x => x.CloudName.Contains(env));
             if (cloud == null)
             {
                 cloud = clouds[0];
             }
 
-            if (!System.IO.File.Exists(cloud.Filename)) {
+            if (!System.IO.File.Exists(cloud.Filename))
+            {
                 UrlModel model = new UrlModel();
                 model.updateCloud(cloud);
             }
 
             var jsonString = System.IO.File.ReadAllText(cloud.Filename);
             return JsonSerializer.Deserialize<ServiceTagsModel>(jsonString, options);
+
         }
-        private ServiceTagsModel getServiceTagsModel() {
+        private ServiceTagsModel getServiceTagsModel()
+        {
 
             string env;
 
-            if (String.IsNullOrEmpty(Request.Cookies[SessionKeyName])) {
+            if (String.IsNullOrEmpty(Request.Cookies[SessionKeyName]))
+            {
                 env = clouds[0].CloudName;
-            } else {
+            }
+            else
+            {
                 env = Request.Cookies[SessionKeyName];
             }
             return getServiceTagsModel(env);
-            
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
