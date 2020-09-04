8e4c1715e348edb860f5af416fca869cd0db4d6a
Daniel Falkner <dafalkne@microsoft.com>
2020-09-04 14:44:05 +0200

    Ajax request fails clear html

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,17 +18,15 @@ $(document).ready(function(){
 
     // Read selected option
     $('#selST').change(function(){
-        //var id = $('#selST option:selected').text();
-        //var val = $('#selST').val();
- 
+       
         var id = '';
         $.each( $('#selST').val(), function( index, value ){
           console.log(index + ": " + value );
           id += value
           if (index < $('#selST').val().length - 1 ) { id += ';'; }
         });
-       
-        var request = $.ajax({
+        
+       var request = $.ajax({
             url: "/getPrefixes/" + $('#downloadlink').data('url') + '/' + id,
             method: "GET",
             dataType: "html"
@@ -44,7 +42,7 @@ $(document).ready(function(){
               servicecnt += service.addressPrefixes.length;
               ranges.push(service.addressPrefixes);
             });
-            if (servicecnt > 0) {
+
               $('#result').html('<p>Found: ' + servicecnt + ' matching IP ranges out of ' + json.length + ' services</p>');
               if (servicecnt > 400)  $('#result').append('<p class="text-danger">UDRs in Azure have a maximum limit of 400 routes</p>');
               $('#result').append('<textarea id="resultarea" class="form-control" rows="' + servicecnt + '"></textarea>'); 
@@ -60,19 +58,19 @@ $(document).ready(function(){
               $('#armdownloadlink').html('<a class="btn btn-light btn-block" href=downloadARMTemplate/' + $('#armdownloadlink').data('url') + '/' + id + '>ARM Download</a>');
               $('#deploytoazure').html('<a class="btn btn-light btn-block" target=_blank href="https://portal.azure.com/#create/Microsoft.Template/uri/' + urlEnc(window.location.origin) + urlEnc('/') + urlEnc('deployARMTemplate/' + $('#deploytoazure').data('url') + '/' + id) + '">Deploy UDR to Azure</a>');
               $('#opensenselink').html('<a class="btn btn-light btn-block" target=_blank href=/getOPNSenseURLTable/' + $('#opensenselink').data('url') + '/' + id + '>OPNsense UrlTable</a>');
-            } else {
-              $('#links').each(function( index ) {
-                $(this).html('');
-              });
-              $('#result').html('');
-              $('#resultarea').html('');
-            }
-            
+                     
 
           });
            
           request.fail(function( jqXHR, textStatus ) {
-            alert( "Request failed: " + textStatus );
+
+            $('#links').each(function( index ) {
+              $(this).html('');
+            });
+            $('#result').html('');
+            $('#resultarea').html('');
+
+            console.log( "Request failed: " + textStatus );
           });
           
 
